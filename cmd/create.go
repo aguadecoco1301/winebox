3ce5fcb687fb78832f4fb1584432174c8a28fad7
Adriel Ulloa <adrielalejoulloa@gmail.com>
2026-05-14 20:54:59 -0300

    Create command, with allowed characters

--- cmd/create.go
+++ cmd/create.go
@@ -1,11 +1,11 @@
 /*
 Copyright © 2026 ADRIEL ULLOA <adrielalejoulloa@gmail.com>
-
 */
 package cmd
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/spf13/cobra"
 )
@@ -14,9 +14,52 @@ import (
 var createCmd = &cobra.Command{
 	Use:   "create",
 	Short: "Creates a new WinePrefix",
-	Long: `Creates a new WinePrefix.`,
+	Long:  `Creates a new WinePrefix. The correct way to use it is to create a WinePrefix for each application you install.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("create called")
+		prefixName := strings.Join(args, " ")
+		var suggestedName string
+		var isUsedCorrectCharacters bool = true
+
+		//TODO: gestionar el nombre (args). Solo alfabeto, - y ., flag para cancelarlo
+		// - Recorrer caracter por caracter (args)
+		flags, err := cmd.Flags().GetBool("allow-unsafe-name")
+		if err != nil {
+			fmt.Println("Error: ", err)
+		}
+		fmt.Println(flags)
+
+		if flags == false {
+			isAllowedCharacters := func(r byte) bool {
+				return r >= 'a' && r <= 'z' || r >= 'A' && r <= 'Z' || r >= '0' && r <= '9' || r == '-' || r == '.'
+			}
+
+			for i := 0; i < len(prefixName); i++ {
+				char := prefixName[i]
+				if char == ' ' {
+					char = '-'
+					isUsedCorrectCharacters = false
+				}
+
+				if isAllowedCharacters(char) {
+					suggestedName += string(char)
+				} else {
+					isUsedCorrectCharacters = false
+				}
+			}
+		} else {
+			fmt.Println("WARNING: Using an unsafe name may cause Wine issues")
+			suggestedName = prefixName
+		}
+
+		if isUsedCorrectCharacters {
+			fmt.Println("New prefix: ", suggestedName) // Reemplazo de la creación del prefix
+		} else {
+			if suggestedName != "" {
+				fmt.Println("ERROR: You entered an unsafe name. No action will be taken.\nIf you know what you are doing, check 'winebox create --help'.\nSuggested safe name:\n", suggestedName)
+			} else {
+				fmt.Println("ERROR: You entered an unsafe name. No action will be taken.\nIf you know what you are doing, check 'winebox create --help'.\nPlease use letters, numbers, '-' or '.'.")
+			}
+		}
 	},
 }
 
@@ -27,9 +70,9 @@ func init() {
 
 	// Cobra supports Persistent Flags which will work for this command
 	// and all subcommands, e.g.:
-	// createCmd.PersistentFlags().String("foo", "", "A help for foo")
+	//createCmd.PersistentFlags().String("foo", "", "A help for foo")
 
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
-	// createCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	createCmd.Flags().BoolP("allow-unsafe-name", "a", false, "Allows any character in the prefix name. May cause errors with Wine.")
 }
